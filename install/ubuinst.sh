047bc0c53a038cde44f2308fded36a2492feef9b
nandoslayer <92729509+nandoslayer@users.noreply.github.com>
2022-06-13 16:34:43 -0300

    Add files via upload

--- install/ubuinst.sh
+++ install/ubuinst.sh
@@ -122,15 +122,16 @@ function dependencias {
   done
 }
 function install_start {
-  if [[ -e "/var/www/html/pages/system/pass.php" ]]; then
-   clear
-    echo -e "\033[1;31mPAINEL JÁ INSTALDO EM SUA VPS, RECOMENDO\033[0m"
-    echo -e "\033[1;31mUMA FORMATAÇÃO PARA UMA NOVA INSTALÇÃO!\033[0m"
-    sleep 5
-    systemctl restart apache2 > /dev/null 2>&1
+if [[ -e "/var/www/html/pages/system/pass.php" ]]; then
+clear
+msg -bar
+echo -e "\033[1;31mPAINEL JÁ INSTALDO EM SUA VPS, RECOMENDO\033[0m"
+echo -e "\033[1;31mUMA FORMATAÇÃO PARA UMA NOVA INSTALÇÃO!\033[0m"
+msg -bar
+sleep 5
+systemctl restart apache2 > /dev/null 2>&1
 cat /dev/null > ~/.bash_history && history -c
 rm /bin/ubuinst* > /dev/null 2>&1
-clear
 exit;
 else
   echo -e 'by: @nandoslayer' >/usr/lib/telegram
